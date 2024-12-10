48eaacddd64713a1fefd82c66e155ae4fe805777
Nathan Thompson <thompn18@email.franklin.edu>
2024-12-09 23:04:32 -0500

    LinkedList, Stack, and Queue initial implementations

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import nathanthomp.datastructures.Queue;
+import nathanthomp.datastructures.Stack;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        testQueue();
+    }
+
+    private static void testStack() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+
+        stack.print();
+
+        int popped = stack.pop();
+    }
+
+    private static void testQueue() {
+        Queue<Integer> queue = new Queue<>();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        int dequeued = queue.dequeue();
+        queue.print();
     }
 }
